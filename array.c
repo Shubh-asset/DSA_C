41230c9394add7a6e19f8c2136513289348b9d58
Shubh-asset <workoho001@gmail.com>
2024-11-11 07:10:48 +0530

    commit

---
+++ array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int size, a[50];
+
+    printf("the size is:");
+    scanf("%d", &size);
+    
+    if (size >50)
+    {
+        printf("size is exceeding the limit");
+    }    
+
+    printf("enter the values of array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("the values are:\n");
+    for(int i=0; i<size; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+    
+
+    return 0;
+}
