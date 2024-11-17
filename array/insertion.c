597e3288c431c0a3ea0a33ba3b0b124f7fa520ba
Shubh-asset <workoho001@gmail.com>
2024-11-17 18:11:17 +0530

    commit

--- array/insertion.c
+++ array/insertion.c
@@ -58,35 +58,108 @@
 
 //Insertion: at the end of the array
 
-    int main()
-    {
+    // int main()
+    // {
          
-        int m, size, num, pos, i;
+    //     int m, size, num, pos, i;
        
+    //     printf("Enter the size of Array(default): ");
+    //     scanf("%d", &m);
+
+    //     int arr[m];
+
+    //     printf("Enter the size of array for your values: ");
+    //     scanf("%d", &size);
+
+    //     if (size>m)
+    //     {
+    //         printf("The size you entered is much larger than defined array!!");
+    //         return 1;
+    //     }
         
         
+    //     printf("Enter the values of your array: \n");
+    //         for( m=0; m<size; m++)
+    //         {
+    //             scanf("%d", &arr[m]);
+    //         }
 
-        printf("Enter the size of Array(default)\n");
-        scanf("%d", &m);
+    //         printf("The entered values are: \n");
+    //         for(i=0; i<size; i++);
+    //         {
+    //             printf("%d\n", arr[i]);
+    //         }
 
-        int arr[m];
+    //     printf("What digit should be entered in array: ");
+    //     scanf("%d", &num);
 
-        printf("Enter the size of array for your values: ");
-        scanf("%d", &size);
+    //     printf("\n**The number will be added at the end**\n");
 
-        if (size>m)
+    //     arr[size] = num;
+    //     size++;
+
+    //     printf("\nthe new array is: \n");
+    //     for(i=0; i<size; i++)
+    //     {
+    //         printf("%d\n", arr[i]);
+    //     }
+        
+    //     return 0;
+    // }
+
+//Insertion: at the begining of an array
+
+
+    int main()
         {
-            printf("The size you entered is much larger than defined array!!");
-        }
+            
+            int m, size, num, pos, i;
         
-        else
-        {
-        printf("Enter the values of your array: ");
-            for( i=0; i<size-1; i++)
+            printf("Enter the size of Array(default): ");
+            scanf("%d", &m);
+
+            int arr[m];
+
+            printf("Enter the size of array for your values: ");
+            scanf("%d", &size);
+
+            if (size>m)
             {
-                scanf("%d", &arr[i]);
+                printf("The size you entered is much larger than defined array!!");
+                return 1;
             }
-        }
+            
+            
+            printf("Enter the values of your array: \n");
+                for( m=0; m<size; m++)
+                {
+                    scanf("%d", &arr[m]);
+                }
 
-        return 0;
-    }
+                printf("The entered values are: \n");
+                for(i=0; i<=size; i++);
+                {
+                    printf("%d\n", arr[i]);
+                }
+
+            printf("What digit should be entered in array: ");
+            scanf("%d", &num);
+
+            printf("\n**The number will be added at the Beginning**\n");
+
+            for ( i = size; i >= 0; i--)
+            {
+                arr[i+1] = arr[i];
+            }
+            
+            arr[0] = num;
+            size++;
+
+            printf("\nthe new array is: \n");
+            for(i=0; i<size; i++)
+            {
+                printf("%d\n", arr[i]);
+            }
+            
+            return 0;
+        }
