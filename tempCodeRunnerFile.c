5d7a13e31dc2b0d36f7746b0678059d4f0b685cc
Shubh-asset <workoho001@gmail.com>
2024-11-27 14:58:56 +0530

    commi

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,48 @@
+int main()
+    // {
+         
+    //     int m, size, num, pos, i;
+       
+    //     printf("Enter the size of Array(default): ");
+    //     scanf("%d", &m);
+
+    //     int arr[m];
+
+    //     printf("Enter the size of array for your values: ");
+    //     scanf("%d", &size);
+
+    //     if (size>m)
+    //     {
+    //         printf("The size you entered is much larger than defined array!!");
+    //         return 1;
+    //     }
+        
+        
+    //     printf("Enter the values of your array: \n");
+    //         for( m=0; m<size; m++)
+    //         {
+    //             scanf("%d", &arr[m]);
+    //         }
+
+    //         printf("The entered values are: \n");
+    //         for(i=0; i<size; i++);
+    //         {
+    //             printf("%d\n", arr[i]);
+    //         }
+
+    //     printf("What digit should be entered in array: ");
+    //     scanf("%d", &num);
+
+    //     printf("\n**The number will be added at the end**\n");
+
+    //     arr[size] = num;
+    //     size++;
+
+    //     printf("\nthe new array is: \n");
+    //     for(i=0; i<size; i++)
+    //     {
+    //         printf("%d\n", arr[i]);
+    //     }
+        
+    //     return 0;
+    // }
